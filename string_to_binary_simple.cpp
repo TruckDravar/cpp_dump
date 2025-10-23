09267d9a41cefc8a75f4cb37933e4d7b157ad3e5
Dravar <pranjalsslathia@gmail.com>
2025-10-23 12:45:22 +0000

    added comments

--- string_to_binary_simple.cpp
+++ string_to_binary_simple.cpp
@@ -6,13 +6,14 @@ using namespace std;
 
 int main(){
 
-    string input;
+    string input; // Input string
 
     cout << "String: ";
     getline(cin, input);
 
+    // Converts string to binary and prints them 
     for(char c : input){
-        cout << bitset<8>(c) << " ";
+        cout << bitset<8>(c) << " "; 
     }
 
     cout << endl;
